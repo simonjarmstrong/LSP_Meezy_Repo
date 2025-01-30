306b92b1fcbc18343467e59b8758c2e8847ffaa1
simonarmstrong <simonarmstrong@localhost>
2025-01-30 16:59:53 -0500

    Second time

---
+++ Spring _2025/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	/**
+	 * Simon John Armstrong
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
